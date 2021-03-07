ea0e7d0c290a3f52bfe699eb53415f2532b6c6c5
Shota <64979430+shota-akizuki@users.noreply.github.com>
2021-03-07 20:32:47 +0900

    useStateを使ってstateを動的に更新する

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ColorfulMessage from './components/ColorfulMessage';
 
 const App = () => {
-  const onClickButton = () => alert();
+  const [num, setNum] = useState(0);
+  const countUp = () => {
+    setNum(num + 1);
+  };
+  const countDown = () => {
+    setNum(num - 1);
+  };
   return (
     <>
       <h1 style={{ color: 'red' }}>こんにちは</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
+      <button onClick={countUp}>+</button>
+      <button onClick={countDown}>-</button>
     </>
   );
 };
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const ColorfulMessage = (props) => {
   //分割代入でprops.colorと書かなくてよくなる
   const { color, children } = props;
+
   const contentStyle = {
     //JSではプロパティ名と値が同じ名前のときはプロパティ名を省略できる
     color,
